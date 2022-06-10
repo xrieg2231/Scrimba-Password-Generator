1d2fa326722f5449d40f81a6874e93d1ae774e15
xrieg2231 <p.machowski@op.pl>
2022-06-10 13:38:57 +0200

    Random character
    Prototype random character selector

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345678!@#$%^&*()_+=-';
-console.log(characters)
+var allChars = [];
+for (var i=32; i<127; i++)
+    allChars.push(String.fromCharCode(i));
+
+function generate(){
+    
+}
